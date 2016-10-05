a11844466fabc9f3e7183d14b5f3743ef24946c9
x0r <spartax96@users.noreply.github.com>
2016-10-05 16:27:59 +0530

    Update Clock.h

--- Clock.h
+++ Clock.h
@@ -11,24 +11,24 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #include <stdio.h>
 #include <mmsystem.h>
 
-#define HOUR_WIDTH			10
-#define OPACITY				180
-#define FACTOR				40
-#define MINUTE_WIDTH		6
-#define SECOND_WIDTH		3
-#define POINT_DIAMETER		4
-#define WIDTH(r)			(r.right-r.left)
-#define HEIGHT(r)			(r.bottom-r.top)
-#define IDM_TOPMOST			2016
-#define IDM_QUIT			1729
-#define IDM_SOUND			1024
-#define IDM_CIRCLE			0xbabe
-#define IDM_RECT			0xcafe
+#define HOUR_WIDTH                    10
+#define OPACITY                       180
+#define FACTOR                        40
+#define MINUTE_WIDTH                  6
+#define SECOND_WIDTH                  3
+#define POINT_DIAMETER                4
+#define WIDTH(r)                      (r.right-r.left)
+#define HEIGHT(r)                     (r.bottom-r.top)
+
+#define IDM_TOPMOST                   0xFACE
+#define IDM_QUIT                      0xBABE
+#define IDM_SOUND                     0xDEAD
+#define IDM_CIRCLE                    0xCAFE
 
 #ifndef M_PI
-#define M_PI				3.141592653589793
+#define M_PI                          3.141592653589793
 #endif
 
 #ifndef RADIAN
-#define RADIAN				0.017453292519943295
+#define RADIAN                        0.017453292519943295
 #endif
